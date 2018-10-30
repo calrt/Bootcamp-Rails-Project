b6dde0f8d5bf77f6456d80529455191b6b4ee4a1
Callum Thompson <callumrthompson@me.com>
2018-10-30 12:46:46 +1100

    implemented basic show and index functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
+    @products = Product.all
   end
 
   def show
@@ -8,6 +12,25 @@ class ProductsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
   end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  # def product_params
+  #   #params.require(:product).permit(:title, :description, :price, :image)
+  #   result = params.require(:product).permit(:title, :description, :price, :image)
+  #   result[:price] = result[:price].to_f * 100.0
+  #   result
+  # end
+
 end
