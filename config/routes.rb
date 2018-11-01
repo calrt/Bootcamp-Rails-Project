7ad986768db9241bf748a1045cb176245d7b8932
Callum Thompson <callumrthompson@me.com>
2018-11-01 11:09:14 +1100

    updated routing for orders, generated orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :orders
+  resources :products do
+    resources :orders, only: [:show, :new, :create]
+  end
   devise_for :users
   root 'products#index'
   # get 'order', to: 'products#order'
   # resources :charges
-  get 'products/:id/order', to: 'charges#order', as: 'order'
-  post 'products/:id/order/confirmation', to: 'charges#create', as: 'charge'
+  get 'products/:id/orders/new', to: 'order#new', as: 'checkout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
