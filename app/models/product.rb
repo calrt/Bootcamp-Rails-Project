974f9cdf3486d245d9bc4d69c164723e0b514b24
Callum Thompson <callumrthompson@me.com>
2018-11-02 12:27:37 +1100

    allow a shipping price of zero

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: { in: 1..200 }
   validates :description, presence: true, length: { in: 10..200 }
   validates :price, numericality: { greater_than: 0 }
-  validates :shipping_price, numericality: { greater_than: 0 }
   validates :stock, numericality: { greater_than: 0 }
   belongs_to :user
   has_many :orders
