9bfb2276d68734cf4b9a3601c4b548de1c097397
cryptokitties0327 <kimberley_tse@yahoo.com.au>
2018-11-02 12:21:01 +1100

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { in: 1..200 }
+  validates :description, presence: true, length: { in: 10..200 }
+  validates :price, numericality: { greater_than: 0 }
+  validates :shipping_price, numericality: { greater_than: 0 }
+  validates :stock, numericality: { greater_than: 0 }
   belongs_to :user
   has_many :orders
   has_one_attached :image
