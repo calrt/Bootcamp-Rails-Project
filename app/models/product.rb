b133fd85dc86e0c54989ffdd3a4d5972e98286b4
Callum Thompson <callumrthompson@me.com>
2018-11-05 11:26:45 +1100

    updated login location and stock validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: { in: 1..200 }
   validates :description, presence: true, length: { in: 10..200 }
   validates :price, numericality: { greater_than: 0 }
-  validates :stock, numericality: { greater_than: 0 }
+  # validates :stock, numericality: { greater_than: 0 }
   belongs_to :user
   has_many :orders
   has_one_attached :image
